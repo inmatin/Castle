fa266a941cc9842041d9f7d305c592f9a9f6131c
Ishtiaque Matin <nurul_matin@hotmail.com>
2022-03-11 23:10:04 -0500

    Add files via upload

---
+++ Castle.java
@@ -0,0 +1,32 @@
+/*
+ * Student Name: Ishtiaque Matin
+ * Lab Professor: Leanne Seaward
+ * Due Date: Sep 24th, 2021
+ * Modified: Sep 24th, 2021
+ * Description: This class will print a castle using ASCII art
+ *
+ */
+
+public class Castle {
+
+	public static void main(String[] args) {
+		
+		//printing the output a castle using ASCII art
+		System.out.println("	  >>>              >>>		");
+		System.out.println("	  |                |		");
+		System.out.println("	= == =           = == =		");
+		System.out.println("	|    |     >>>   |    |		");
+		System.out.println("	| [] |     |     | [] |		");
+		System.out.println("	|    |= = = = = =|    |		");
+		System.out.println("	| [] | []     [] | [] |		");
+		System.out.println("	|    |    ===    |    |		");
+		System.out.println("	| [] |   |   |   | [] |		");
+		System.out.println("	|    |   |   |   |    |		");
+		System.out.println("	~~~~~~~~~/   /~~~~~~~~~~	");
+		System.out.println("	~~~~~~~~/   /~~~~~~~~~		");
+		System.out.println("	       /   /				");
+		System.out.println("	Program by Ishtiaque Matin	");
+
+	}
+
+}
